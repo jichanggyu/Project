a696e6891d30f9d94b75d530ba019c525fca88e0
changgyu <sa3590@naver.com>
2019-10-06 20:40:09 +0900

    add linkedlist

--- eclipse/DataStructure/src/list/LinkedList_s.java
+++ eclipse/DataStructure/src/list/LinkedList_s.java
@@ -38,6 +38,67 @@ public class LinkedList_s {
 			public String toString() {
 				return String.valueOf(this.data);
 			}
+			
+			
+		}
+		
+		Node node(int index) {
+			Node x = head;
+			for (int i = 0; i < index; i++) {
+				x = x.next;
+			}
+			return x;
+		}
+		
+		void addFirst(Object input) {
+			Node newNode = new Node(input);
+			newNode.next = head;
+			head = newNode;
+			size++;
+			
+			if(head.next == null) {
+				tail = head;
+			}
+		}
+		
+		void addLast(Object input) {
+			Node newNode = new Node(input);
+			if(size == 0) {
+				addFirst(input);
+			} else {
+				newNode.next = tail;
+				tail = newNode;
+				size++;
+			}
+		}
+		
+		void add(int k, Object input) {
+			if( k == 0 ) {
+				addFirst(input);
+			}
+			else {
+				Node temp1 = node(k-1);
+				Node temp2 = temp1.next;
+				Node newNode = new Node(input);
+				temp1.next = newNode;
+				newNode.next = temp2;
+				size++;
+				
+				if(newNode.next==null) {
+					tail = newNode;
+				}
+			}
+		}
+		
+		Object removeFirst() {
+			Node newNode = head;
+			head = head.next;
+			
+			Object tmp = newNode.data;
+			newNode = null;
+			size--;
+			
+			return tmp;
 		}
 	}
 }// end of class
